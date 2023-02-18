95e09e116d15e34785f340fad56d9055f472e096
Tiffany Dinh <tiffany.q.dinh@gmail.com>
2023-02-17 23:15:25 -0600

    The application now displays three development maps next to one another.

--- src/components/Map/MainPage.js
+++ src/components/Map/MainPage.js
@@ -4,69 +4,67 @@ import { Fragment } from "react/cjs/react.production.min";
 import Card from "../UI/Card";
 import classes from './MapContainer.module.css';
 
-
 const MainPage = () => {
     return (
       <Fragment>
         <ToolSummary />
   
         <p></p>
-  
         <Card className={classes.cardContainer}>
-          <section className={classes.container}>
-            <h3>Map 1: Dr. Mukherjee FM Based Localization System</h3>
+	    <section className={classes.mapContainer}>
+	    <h2>Map 1: Dr. Mukherjee FM Based Localization System</h2>
             <Map
               path={[
-                { lat: 34.719, lng: -86.666 },
-                { lat: 21.291, lng: -157.821 },
-                { lat: -18.142, lng: 178.431 },
-                { lat: -27.467, lng: 153.027 },
+                { lat: 33.719, lng: -86.666 },
+                { lat: 20.291, lng: -157.821 },
+                { lat: -19.142, lng: 178.431 },
+                { lat: -28.467, lng: 153.027 },
               ]}
               color={"blue"}
-              id="Map1"
+              id="Map0"
             />
-          </section>
-  
-          <section className={classes.container}>
-            <h3>Map 2: SoO Localization System</h3>
+	    </section> 
+    
+	    <section className={classes.mapContainer}>
+	    <h2>Map 2: SoO Localization System</h2>
             <Map
               path={[
-                { lat: 34.719, lng: -86.666 },
-                { lat: 21.291, lng: -157.821 },
-                { lat: -18.142, lng: 178.431 },
-                { lat: -27.467, lng: 153.027 },
+                { lat: 33.719, lng: -86.666 },
+                { lat: 20.291, lng: -157.821 },
+                { lat: -19.142, lng: 178.431 },
+                { lat: -28.467, lng: 153.027 },
               ]}
               color={"red"}
-              id="Map2"
+              id="Map1"
             />
-          </section>
-  
-          <section className={classes.container}>
-            <h3>Map 3: SoO Localization with Dead Reckoning applied</h3>
+	    </section> 
+         
+	    <section className ={classes.mapContainer}> 
+	    <h2>Map 3: SoO Localization with Dead Reckoning applied</h2>
             <Map
               path={[
-                { lat: 34.719, lng: -86.666 },
-                { lat: 21.291, lng: -157.821 },
-                { lat: -18.142, lng: 178.431 },
-                { lat: -27.467, lng: 153.027 },
+                { lat: 33.719, lng: -86.666 },
+                { lat: 20.291, lng: -157.821 },
+                { lat: -19.142, lng: 178.431 },
+                { lat: -28.467, lng: 153.027 },
               ]}
               color={"green"}
-              id="Map3"
+              id="Map2"
             />
-          </section>
+	   </section>
         </Card>
+	
 
         <p></p>
-
-        <Card>
+        <Card className={classes.cardContainer}>
         <section className={classes.container}>
-            <h3>Map 4: All Vectors Combined</h3>
+            <h2>Map 4: All Vectors Combined</h2>
             <Map
               path={[
-                { lat: 34.719, lng: -86.666 },
-                { lat: 21.291, lng: -157.821 },
-                { lat: -18.142, lng: 178.431 },
-                { lat: -27.467, lng: 153.027 },
+                { lat: 33.719, lng: -86.666 },
+                { lat: 20.291, lng: -157.821 },
+                { lat: -19.142, lng: 178.431 },
+                { lat: -28.467, lng: 153.027 },
               ]}
               color={"green"}
               id="Map4"
